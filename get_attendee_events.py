b8859db9f934135c78991d995eb78af3502700bc
jnash10 <agamdeep20@iiserb.ac.in>
2025-07-20 02:37:48 +0530

    time proposal done

--- get_attendee_events.py
+++ get_attendee_events.py
@@ -7,11 +7,24 @@ def get_attendee_events(proposed_time, user_email):
     1. the day of the propose start_time
     2. the next day of the proposed end_time"""
     user = user_email
+    start_time = proposed_time["start_time"]
+    end_time = proposed_time["end_time"]
+
+    events = retrive_calendar_events(user, start_time, end_time)
+
+    return events
+
+
+def get_attendee_events_2_days(proposed_time, user_email):
+    """get the attendee's calendar events for:
+    1. the day of the propose start_time
+    2. the next day of the proposed end_time"""
+    user = user_email
     start_day = proposed_time["start_time"].split("T")[0]
     end_day = proposed_time["end_time"].split("T")[0]
     end_day_plus_one = proposed_time["end_time"].split("T")[0]
     end_day_plus_one = end_day_plus_one.split("-")
-    end_day_plus_one[2] = str(int(end_day_plus_one[2]) + 1)
+    end_day_plus_one[2] = str(int(end_day_plus_one[2]) + 0)
     end_day_plus_one = "-".join(end_day_plus_one)
 
     start_day_time_stamp = f"{start_day}T00:00:00+05:30"
@@ -26,7 +39,7 @@ def get_attendee_events(proposed_time, user_email):
 
 
 def get_all_attendee_events(proposed_time, input_request):
-    """Get all attendees' calendar events for the proposed time window's day plus the next day."""
+    """Get all attendees' calendar events for the proposed time window."""
     events = {}
     # get sender events
     from_email = input_request["From"]
@@ -35,4 +48,17 @@ def get_all_attendee_events(proposed_time, input_request):
     for attendee in input_request["Attendees"]:
         user_email = attendee["email"]
         events[user_email] = get_attendee_events(proposed_time, user_email)
+    return events
+
+
+def get_all_attendee_events_2_days(proposed_time, input_request):
+    """Get all attendees' calendar events for the proposed time window's day plus the next day."""
+    events = {}
+    # get sender events
+    from_email = input_request["From"]
+    events[from_email] = get_attendee_events_2_days(proposed_time, from_email)
+    # get all other attendees events
+    for attendee in input_request["Attendees"]:
+        user_email = attendee["email"]
+        events[user_email] = get_attendee_events_2_days(proposed_time, user_email)
     return events
